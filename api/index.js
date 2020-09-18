aea2984ea5a3ad481e5ace4e8a549d490d4c574c
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-18 17:33:34 +0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+module.exports = app
+
+if (require.main === module) {
+  const port = process.env.PORT || 3001
+  app.listen(port, () => {
+    console.log(`API server listening on port ${port}`)
+  })
+}
