6408361e26a1d81fd70c09f62880ed2dc59764bb
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-18 20:27:32 +0300

    added home page

---
+++ components/main/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="post"></div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.post {
+  height: 200px;
+  border: 1px solid black;
+  margin-bottom: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="container"></div>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <app-post v-for="post in 3" :key="post" />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {}
+import AppPost from '@/components/main/Post'
+export default {
+  components: {
+    AppPost,
+  },
+  head: {
+    title: 'Home',
+  },
+}
 </script>
 
 <style></style>
