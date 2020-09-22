8dbefebe688af232960fff9075b3a869e234d329
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-22 18:07:56 +0300

    added login page

---
+++ layouts/empty.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="empty-layout">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyLayout',
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.empty-layout {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 4rem;
+}
+</style>
