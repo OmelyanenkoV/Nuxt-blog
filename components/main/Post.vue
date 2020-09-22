b2eec3937b38c1d683e130e85bd351d2230da94b
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-22 11:12:21 +0300

    added post component

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="post"></div>
+  <el-card class="post" shadow="hover" :body-style="{ padding: '0px' }">
+    <header slot="header" class="post__header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-date"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post__body">
+      <img
+        src="https://img-a.udemycdn.com/course/750x422/982344_7cfa_2.jpg"
+        alt="post image"
+        class="post__body__img"
+      />
+    </div>
+    <footer class="post__footer">
+      <el-button round @click="openPost">Открыть пост</el-button>
+      <span>
+        <i class="el-icon-s-comment"></i>
+        24
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
 export default {
   name: 'Post',
   components: {},
+  methods: {
+    openPost() {
+      console.log('Open')
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .post {
-  height: 200px;
-  border: 1px solid black;
-  margin-bottom: 2rem;
+  margin-bottom: 1.5rem;
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__body {
+    &__img {
+      width: 100%;
+    }
+  }
+
+  &__footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    i {
+      margin-right: 5px;
+    }
+  }
 }
 </style>
