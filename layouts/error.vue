adad41c71e94ef09afaedef6bb5716abbfd48a9f
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-22 12:11:44 +0300

    error layout

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="error">
+    <div class="error__header">
+      <h1>&#128549; 404 Not Found</h1>
+    </div>
+    <nuxt-link to="/">Go to home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.error {
+  margin-top: 12rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
