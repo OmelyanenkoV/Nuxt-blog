65eabd8957ef8df396397d9e4e5b5813ba459936
vlad omelyanenko <shield.deffence@gmail.com>
2020-09-30 14:47:27 +0300

    added passport library

--- api/app.js
+++ api/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const passport = require('passport')
 
 const authRoutes = require('./routes/auth.routes')
 const keys = require('./keys')
+const passportStrategy = require('./middleware/passport-strategy')
 
 const app = express()
 
@@ -13,6 +15,8 @@ mongoose
   .then(() => console.log('MongoDB connected....'))
   .catch((error) => console.error(error))
 
+app.use(passport.initialize())
+passport.use('passportStrategy')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
